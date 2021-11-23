4bce3c48251d8f955804cf28689c481d70dd5a93
Ryota Enoki <ryoenoki2013@gmail.com>
2021-11-24 01:49:09 +0900

    modified booking controller show and added @message, added create method to message controller, changed Meesage model belongs to sender instead of messageable, added chatroom html partial and render for show page

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,21 @@
 class MessagesController < ApplicationController
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @message = Message.new(message_params)
+    @message.booking = @booking
+    @message.sender = current_user
+    authorize @message
+
+    if @message.save
+      redirect_to booking_path(@booking)
+    else
+      render 'bookings/show'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
