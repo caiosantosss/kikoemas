1b0dc6696482d36e2839fbaa9fd6aec1dcf5e89a
Vivian Bow <vivianbao1009@gmail.com>
2021-11-23 16:26:30 +0900

    Enum error fixed, HP basic structure created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user_or_counselor!
+  class AuthorizationException < StandardError
+  end
+
+  rescue_from AuthorizationException do
+    render text: "Access Denied", status: :unauthorized
+  end
+
+  protected
+
+  def authenticate_user_or_counselor!
+    unless user_signed_in? || counselor_signed_in?
+      raise AuthorizationException.new
+    end
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  # skip_before_action :authenticate_user_or_counselor!, only: [:home]
+  # before_action :authenticate_user!
 
   def home
+    @booking = Booking.new
   end
 end
