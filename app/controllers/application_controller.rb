7afd5da0b815f867abe54de8d64357853d34f9a0
Karen Joy Fujibayashi <karendulca22@yahoo.com>
2021-11-24 16:49:45 +0900

    plicy fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,8 +30,13 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(user)
     if user.is_a?(Counselor)
       counselor_dashboard_path
+      # raise
     else
       root_path
     end
+  end
+
+  def pundit_user
+    current_counselor || current_user
   end
 end
