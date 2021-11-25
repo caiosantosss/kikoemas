a2988f92af296c9a0b2d35c82576cade6383a455
Caio Santos <caio.santos@msn.com>
2021-11-25 18:33:58 +0900

    started the Upcoming this week

--- app/controllers/counselors_controller.rb
+++ app/controllers/counselors_controller.rb
@@ -8,5 +8,7 @@ class CounselorsController < ApplicationController
     @new_bookings = current_counselor.bookings.where("date_part('hour', created_at) = ?", Time.now.hour)
     @upcoming = current_counselor.bookings.where('start_time > ?', Time.now)
     @past = current_counselor.bookings.where('start_time < ?', Time.now)
+    @today = current_counselor.bookings.where(start_time: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    @this_week = current_counselor.bookings.where('start_time BETWEEN ? AND ?', Time.now.beginning_of_week, Time.now.end_of_week)
   end
 end
