a3de33a010841b0d1c65a2acbdbc267bf47cfdb8
Caio Santos <caio.santos@msn.com>
2021-11-25 12:36:26 +0900

    separeted user booking and counselor booking

--- app/controllers/counselors_controller.rb
+++ app/controllers/counselors_controller.rb
@@ -3,4 +3,10 @@ class CounselorsController < ApplicationController
     @counselor = current_counselor
     authorize current_counselor
   end
+
+  def bookings
+    @new_bookings = current_counselor.bookings.where('hour(created_at) = ?', Time.now.hour)
+    @upcoming = current_counselor.bookings.where('start_time > ?', Time.now)
+    @past = current_counselor.bookings.where('start_time < ?', Time.now)
+  end
 end
