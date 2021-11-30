556675907fc68a00ef3c08ecf7ceef8009f61d38
Ryota Enoki <ryoenoki2013@gmail.com>
2021-11-30 16:01:53 +0900

    emotions

--- app/helpers/daily_emotion_helper.rb
+++ app/helpers/daily_emotion_helper.rb
@@ -2,10 +2,10 @@ module DailyEmotionHelper
   def fontawesome(emotion)
       case emotion
       when 'Sad' then "far fa-frown"
-      when 'Very Sad' then "far fa-frown"
+      when 'Very Sad' then "far fa-sad-tear"
       when 'Neutral' then "far fa-meh"
-      when 'Happy' then "far fa-frown"
-      when 'Very Happy' then "far fa-frown"
+      when 'Happy' then "far fa-smile"
+      when 'Very Happy' then "far fa-laugh-beam"
       end
     end
 end
